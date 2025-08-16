11cdd30279aea863902d2f236a0ed8f93370b5fb
aswinganga-greeninkpro <aswinganga.greeninkpro@gmail.com>
2025-08-16 15:42:30 +0530

    change/added a new dag

---
+++ dags/spark-job-test-2.py
@@ -0,0 +1,32 @@
+K8S_CONN_ID = "k8s_conn_id"
+
+# Create the AIRFLOW_CONN_* env var
+os.environ[f"AIRFLOW_CONN_{K8S_CONN_ID.upper()}"] = json.dumps({
+    "conn_type": "kubernetes",
+    "extra": {
+        "in_cluster": True,
+        "namespace": "test"  # Default namespace for this connection
+    }
+})
+
+# DAG definition
+with DAG(
+    dag_id="run_pyspark_test(2)",
+    start_date=datetime(2025, 8, 15),
+    schedule_interval=None,  # Run manually for now
+    catchup=False,
+    tags=["pyspark", "k8s"],
+) as dag:
+
+    run_pyspark_job = KubernetesPodOperator(
+        task_id="run_pyspark_job(2)",
+        name="pyspark-test-pod(2)",
+        labels={"app": "pyspark-job", "team": "data"},
+        namespace="test",  # Runs in namespace test
+        service_account_name="dagsvc",
+        image="ghcr.io/vishnu-thirumangalath/docker-images/pyspark-dags-test:latest",
+        cmds=["python", "/app/pyspark_test.py"],  # matches dockerfile (refer Dockerfile)
+        get_logs=True,
+        is_delete_operator_pod=False,  # pod termination
+        # kubernetes_conn_id="k8s_conn_id"
+    )
