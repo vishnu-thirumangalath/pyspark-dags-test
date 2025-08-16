f252e845eeafd8c212966b0ca0e34edcd3f90182
aswinganga-greeninkpro <aswinganga.greeninkpro@gmail.com>
2025-08-16 15:30:41 +0530

    change/all will run in same namespace now

--- dags/spark-job-test.py
+++ dags/spark-job-test.py
@@ -5,16 +5,16 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 from datetime import datetime
 
 # Define the connection ID
-K8S_CONN_ID = "k8s_conn_id"
+# K8S_CONN_ID = "k8s_conn_id"
 
 # Create the AIRFLOW_CONN_* env var
-os.environ[f"AIRFLOW_CONN_{K8S_CONN_ID.upper()}"] = json.dumps({
-    "conn_type": "kubernetes",
-    "extra": {
-        "in_cluster": True,
-        "namespace": "test"  # Default namespace for this connection
-    }
-})
+# os.environ[f"AIRFLOW_CONN_{K8S_CONN_ID.upper()}"] = json.dumps({
+#     "conn_type": "kubernetes",
+#     "extra": {
+#         "in_cluster": True,
+#         "namespace": "test"  # Default namespace for this connection
+#     }
+# })
 
 # DAG definition
 with DAG(
@@ -29,11 +29,11 @@ with DAG(
         task_id="run_pyspark_job",
         name="pyspark-test-pod",
         labels={"app": "pyspark-job", "team": "data"},
-        namespace="test",  # Runs in namespace test
-        service_account_name="dagsvc",
+        namespace="airflow",  # Runs in namespace test
+        service_account_name="airflow-webserver",
         image="ghcr.io/vishnu-thirumangalath/docker-images/pyspark-dags-test:latest",
         cmds=["python", "/app/pyspark_test.py"],  # matches dockerfile (refer Dockerfile)
         get_logs=True,
         is_delete_operator_pod=False,  # pod termination
-        kubernetes_conn_id="k8s_conn_id"
+        # kubernetes_conn_id="k8s_conn_id"
     )
